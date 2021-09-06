7152c56f3fdb0d9481bee3ce17cd5ba1caca3024
yosofbayan <65795344+yosofbayan@users.noreply.github.com>
2021-09-06 14:03:26 +0300

    lAST update

--- app/src/main/java/com/yb/doctors/DoctorInfo.java
+++ app/src/main/java/com/yb/doctors/DoctorInfo.java
@@ -10,6 +10,6 @@ public class DoctorInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_info);
-        //yosof editing 
+        //yosof editing
     }
 }
