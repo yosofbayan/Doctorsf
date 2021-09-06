0208223194cbecbe854496b5c0a3c16079e6b70d
Marwan00 <marwan.shaheen223@gmail.com>
2021-09-06 12:40:39 +0300

    hello

---
+++ app/src/main/java/com/yb/doctors/DoctorInfo.java
@@ -0,0 +1,14 @@
+package com.yb.doctors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoctorInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_info);
+    }
+}
