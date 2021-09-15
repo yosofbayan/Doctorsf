f5ab36e5c5fae9b1ae6fc9b692f275e9093dafe0
Marwan Shaheen <marwan.shaheen223@gmail.com>
2021-09-15 21:42:40 +0000

    Update MainPageActivity.java

--- app/src/main/java/com/yb/doctors/MainPageActivity.java
+++ app/src/main/java/com/yb/doctors/MainPageActivity.java
@@ -1,13 +1,9 @@
 package com.yb.doctors;
 
 
-
 import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
-
-import android.net.Uri;
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Gravity;
@@ -18,7 +14,6 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -27,6 +22,13 @@ import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.navigation.NavigationView;
 import com.yb.doctors.adapters.HealthCentersAdapter;
 import com.yb.doctors.adapters.PhotoAdapter;
+import com.yb.doctors.adapters.SectorsAdapter;
+import com.yb.doctors.adapters.TopsAdapter;
+import com.yb.doctors.model.Doctor;
+import com.yb.doctors.model.FourHealthCenterObjects;
+import com.yb.doctors.model.Sector;
+import androidx.drawerlayout.widget.DrawerLayout;
+import java.util.ArrayList;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
@@ -38,21 +40,9 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.ListResult;
 import com.google.firebase.storage.StorageReference;
 
-import com.yb.doctors.adapters.SectorsAdapter;
-import com.yb.doctors.adapters.TopsAdapter;
-import com.yb.doctors.model.Doctor;
-import com.yb.doctors.model.FourHealthCenterObjects;
-import com.yb.doctors.model.Sector;
-import androidx.drawerlayout.widget.DrawerLayout;
-import java.util.ArrayList;
-import java.util.Map;
 
 
 public class MainPageActivity extends AppCompatActivity {
-    ArrayList<String> imagelist;
-    StorageReference root;
-    /*private void collectPhoneNumbers(Map<String,Object> users) {
-
 
     private RecyclerView specialtiesRecyclerView,topRecyclerView;
     private final Handler sliderHandler = new Handler();
@@ -69,6 +59,26 @@ public class MainPageActivity extends AppCompatActivity {
     private TopsAdapter topsAdapter;
     private HealthCentersAdapter healthCentersAdapter;
     private DrawerLayout drawerLayout;
+    ArrayList<String> imagelist;
+    StorageReference root;
+
+
+     /*private void collectPhoneNumbers(Map<String,Object> users) {
+
+        ArrayList<String> phoneNumbers = new ArrayList<>();
+
+        //iterate through each user, ignoring their UID
+        for (Map.Entry<String, Object> entry : users.entrySet()){
+
+            //Get user map
+            Map singleUser = (Map) entry.getValue();
+            //Get phone field and append to list
+            phoneNumbers.add(singleUser.get("Name_AR").toString());
+        }
+
+        System.out.println("HI   " +phoneNumbers.toString());
+    }*/
+
 
 
     @Override
@@ -82,6 +92,23 @@ public class MainPageActivity extends AppCompatActivity {
         setTopsAdapter();
         setHealthCentersAdapter();
         setOnClickListeners();
+
+            /*private void collectPhoneNumbers(Map<String,Object> users) {
+
+        ArrayList<String> phoneNumbers = new ArrayList<>();
+
+        //iterate through each user, ignoring their UID
+        for (Map.Entry<String, Object> entry : users.entrySet()){
+
+            //Get user map
+            Map singleUser = (Map) entry.getValue();
+            //Get phone field and append to list
+            phoneNumbers.add(singleUser.get("Name_AR").toString());
+        }
+
+        System.out.println("HI   " +phoneNumbers.toString());
+    }*/
+
     }
 
     public void initializeVariables(){
@@ -186,116 +213,6 @@ public class MainPageActivity extends AppCompatActivity {
         //Temp Data
         photos= new ArrayList();
 
-        ArrayList<String> phoneNumbers = new ArrayList<>();
-
-        //iterate through each user, ignoring their UID
-        for (Map.Entry<String, Object> entry : users.entrySet()){
-
-            //Get user map
-            Map singleUser = (Map) entry.getValue();
-            //Get phone field and append to list
-            phoneNumbers.add(singleUser.get("Name_AR").toString());
-        }
-
-        System.out.println("HI   " +phoneNumbers.toString());
-    }*/
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page);
-        /*
-        //Get datasnapshot at your "users" root node
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Home").child("specialties");
-        ref.addListenerForSingleValueEvent(
-                new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        //Get map of users in datasnapshot
-                        collectPhoneNumbers((Map<String,Object>) dataSnapshot.getValue());
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                        //handle databaseError
-                    }
-                });
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        imagelist=new ArrayList<>();
-        RecyclerView recyclerView = findViewById(R.id.main_page_specialties_recycler_view);
-        SectorsAdapter sectorsAdapter =new SectorsAdapter(imagelist);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainPageActivity.this,RecyclerView.HORIZONTAL,false);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        StorageReference listRef = FirebaseStorage.getInstance()
-                .getReference().child("images");
-        listRef.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
-            @Override
-            public void onSuccess(ListResult listResult) {
-                for(StorageReference file:listResult.getItems()){
-                    file.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                        @Override
-                        public void onSuccess(Uri uri) {
-                            imagelist.add(uri.toString());
-                            Log.e(" Itemvalue : " , uri.toString());
-                        }
-                    }).addOnSuccessListener(new OnSuccessListener<Uri>() {
-                        @Override
-                        public void onSuccess(Uri uri) {
-                            recyclerView.setAdapter(sectorsAdapter);
-                        }
-                    });
-                }
-            }
-        });
-
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*
-        ArrayList sectors = new ArrayList();
-
-
         for(int i=0 ; i < 10 ; i++)
             photos.add("");
 
@@ -314,7 +231,7 @@ public class MainPageActivity extends AppCompatActivity {
         });
 
 
-*/
+
     }
 
 
@@ -445,4 +362,3 @@ public class MainPageActivity extends AppCompatActivity {
    }
 
 }
-
